a800f388349d46313176d1bf71c846e0907b31d0
Tuomo Makkonen <tuomo.makkonen@iki.fi>
2011-01-11 21:58:34 +0200

    Initial commit of my helloword RoR app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloapp::Application.config.secret_token = '5c8c64d2b539a680249683bf2b0641c958d588969155a715e94d8ce5fa1ce689c1476e7bff3845437fcfa48f74ff702406e7f0e60915254a3cdfb8e7858a48ad'
