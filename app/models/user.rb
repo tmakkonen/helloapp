628aa632d7760d07af71f748e9a772f658f453de
Tuomo Makkonen <tuomo.makkonen@iki.fi>
2011-02-03 23:46:25 +0200

    added signin stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,17 @@ class User < ActiveRecord::Base
 		encrypted_password == encrypt(submitted_password)
 	end
 
+	def self.authenticate(email, password)
+		user = find_by_email(email)
+		return nil if user.nil?
+		return user if user.has_password?(password)
+	end
+
+	def self.authenticate_with_salt(id, cookie_salt)
+		user = find_by_id(id)
+		(user && user.salt == cookie_salt) ? user : nil
+	end
+
 	private
 		def encrypt_password
 			self.salt = make_salt if new_record?
